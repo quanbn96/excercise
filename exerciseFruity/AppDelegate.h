09feb09177df43c9341518c16ec4533bacabfcbf
quanbn96 <qdbn122@gmail.com>
2016-08-17 14:49:35 +0700

    Initial Commit

---
+++ exerciseFruity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  exerciseFruity
+//
+//  Created by Quan on 8/17/16.
+//  Copyright © 2016 Fruity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
